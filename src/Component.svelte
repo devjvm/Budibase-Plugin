c34e8bae1575f5f1e1fb2d081a66467a6248d43c
Joao Mendes <j.mendes@cermob.com.br>
2025-04-03 14:59:33 -0300

    Escrita botão

--- src/Component.svelte
+++ src/Component.svelte
@@ -374,7 +374,7 @@
       />
       <input
         type="button"
-        value="Browse..."
+        value="Anexar"
         class="browse-button"
         on:click={() => document.getElementById(buttonID).click()}
         disabled={fieldState.disabled}
@@ -410,12 +410,7 @@
           {#if selectedFile.type.startsWith('image/')}
             <img src={blobFiles[selectedFileIdx]} alt={selectedFile.name} class="thumbnail" />
           {:else}
-            <div class="placeholder">
-              <div class="extension">
-                {selectedFile.name || "Unknown file"}
-              </div>
-              <div>Preview not supported</div>
-            </div>
+            <embed src={blobFiles[selectedFileIdx]} alt={selectedFile.name} class="thumbnail" />
           {/if}
           <div
           class="nav left"
